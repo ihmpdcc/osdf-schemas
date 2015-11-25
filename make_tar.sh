bc810493715fe6276db656f373502d15f5c2b102
Victor <victor73@gmail.com>
2015-11-25 14:48:48 -0500

    Taking top commit.

--- make_tar.sh
+++ make_tar.sh
@@ -7,7 +7,7 @@
 git pull
 
 # Get the last revision so we can make a versioned tarball
-REVISION=$(git log --abbrev-commit --pretty=oneline | sed -e "s:\x1B\[[0-9;]*[mK]::g" | awk '{print $1}')
+REVISION=$(git log --abbrev-commit --pretty=oneline | sed -e "s:\x1B\[[0-9;]*[mK]::g" | awk '{print $1}' | head -n 1)
 
 # Create the tar file
 tar -zcv --exclude='.git' --exclude='.gitignore' -f ihmp-$REVISION.tar.gz ihmp 
